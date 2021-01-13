cbe2e02db15e75593dd505eb5e290d68814d7dc3
Josh Ganote <j.ganote0825@gmail.com>
2021-01-13 17:36:55 -0600

    inital project setup

---
+++ server/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AngularUniversalModule } from '@nestjs/ng-universal';
+import { join } from 'path';
+import { AppServerModule } from '../src/main.server';
+import { TestController } from './test/test.controller';
+
+@Module({
+  imports: [
+    AngularUniversalModule.forRoot({
+      bootstrap: AppServerModule,
+      viewsPath: join(process.cwd(), 'dist/NestJS-OAuth/browser')
+    })
+  ],
+  controllers: [TestController]
+})
+export class AppModule {}
