4edc433ed28e461d8e089d606a322b2782af3995
Josh Ganote <j.ganote0825@gmail.com>
2021-01-15 13:50:14 -0600

    cleaning up component

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { IFormData } from '../../services/data/IFormData';
 import { TestService } from '../../services/test.service';
@@ -12,7 +12,8 @@ import { TestService } from '../../services/test.service';
 export class UserFormComponent implements OnInit {
   userForm: FormGroup;
   userData$: Observable<IFormData>;
-
+  validBlogUrl: string =
+    '^((https?|ftp|smtp)://)?(www.)?[a-z0-9]+.[a-z]+(/[a-zA-Z0-9#]+/?)*$';
   constructor(private fb: FormBuilder, private test: TestService) {}
 
   ngOnInit(): void {
@@ -20,17 +21,21 @@ export class UserFormComponent implements OnInit {
     console.log(this.userData$);
     this.userForm = this.fb.group({
       name: '',
-      email: '',
-      blog: '',
+      email: ['', [Validators.required, Validators.email]],
+      blog: ['', [Validators.required, Validators.pattern(this.validBlogUrl)]],
       twitter: '',
       company: '',
       location: '',
       hireable: '',
-      bio: '',
+      bio: ['', [Validators.required, Validators.minLength(160)]],
     });
   }
 
-  onSubmit() {
-    console.log(this.userForm.value);
+  get formValidators() {
+    return {
+      email: this.userForm.get('email'),
+      blog: this.userForm.get('blog'),
+      bio: this.userForm.get('bio'),
+    };
   }
 }
--- src/app/services/data/mock-data.ts
+++ src/app/services/data/mock-data.ts
@@ -3,7 +3,7 @@ import { IFormData } from '../data/IFormData';
 export const FormData: IFormData = {
   name: 'Josh Ganote',
   email: 'jg@gmail.com',
-  blog: 'You miss 100% of the shots you miss',
+  blog: 'https://github.blog/author/joshganote/',
   twitter: '@jg22',
   company: 'Artisan',
   location: 'Kansas City',
