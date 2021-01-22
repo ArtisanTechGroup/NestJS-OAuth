d140199a91592a7a2e5c3aeb05fd5d58d6947d8d
Josh Ganote <j.ganote0825@gmail.com>
2021-01-22 10:22:45 -0600

    added route for user form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: UserFormComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
-})],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
