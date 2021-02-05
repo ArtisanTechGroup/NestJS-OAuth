4391b9f4398b9be9ba5109e0eb5895d1203ac56e
Josh Ganote <j.ganote0825@gmail.com>
2021-02-05 15:17:11 -0600

    adding new variables

--- server/Config/Configuration.ts
+++ server/Config/Configuration.ts
@@ -1,4 +1,8 @@
 export const config = ({
   defaultHost: process.env.DEFAULT_URI,
   defaultPort: process.env.EXPRESS_PORT,
+  personalGitHubToken: process.env.PERSONAL_ACCESS_TOKEN,
+  defaultGitHubUri: process.env.DEFAULT_GITHUB_URI,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
 });
